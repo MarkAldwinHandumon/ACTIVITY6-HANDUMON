a5533a4f45b293d06a3433c245f2eb6326bb81c9
MarkAldwinHandumon <143053201+MarkAldwinHandumon@users.noreply.github.com>
2023-11-22 16:27:53 -0800

    ACTT6

---
+++ src/User.jsx
@@ -0,0 +1,123 @@
+export const User = [
+    {
+      id: 1,
+      name: {
+        firstname: "JOHNBERT",
+        lastname: "MAGALONA",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "BAYOT",
+      password: "6969",
+      usertype: "STUDENT",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "JOHN CARLO",
+        lastname: "NAMOR",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "GEEK",
+      password: "1000",
+      usertype: "STUDENT",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "NILSON",
+        lastname: "RABANES",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "VICERAL",
+      password: "1293",
+      usertype: "STUDENT",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "FLORA MAE",
+        lastname: "REQUILLO",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "FLOWER",
+      password: "1234",
+      usertype: "STUDENT",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "JAMES",
+        lastname: "ESTOCADO",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "YORME",
+      password: "23214",
+      usertype: "STUDENT",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "PEARL",
+        lastname: "NOBLE",
+      },
+      course: "BSIT",
+      yearsection: "3B",
+      username: "DEANSMART",
+      password: "124523",
+      usertype: "STUDENT",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "JEAN VAN",
+        lastname: "GRAN",
+      },
+      course: "",
+      yearsection: "",
+      username: "JCVG",
+      password: "92378",
+      usertype: "TEACHER",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "ASUNCION",
+        lastname: "PANA",
+      },
+      course: "",
+      yearsection: "",
+      username: "MAMPROB",
+      password: "7888",
+      usertype: "TEACHER",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "JERRY",
+        lastname: "ALE",
+      },
+      course: "",
+      yearsection: "",
+      username: "GWAPOSIR",
+      password: "5699",
+      usertype: "TEACHER",
+    },
+    {
+      id: 10,
+      name: {
+        firstname: "BENJAMIN",
+        lastname: "OMAMALIN",
+      },
+      course: "",
+      yearsection: "",
+      username: "HALAKAWOW",
+      password: "8710",
+      usertype: "TEACHER",
+    },
+    
+  ];
